7a25d27ba8ee9af176a8b714f5185b0d711515f6
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-26 14:25:44 +0200

    get value from user input and alert user with message and name

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,5 +1,2 @@
-let someString = "one string";
-let someNumber = 32;
-let someBoolean = true;
-const someNull = null;
-let someUndefined = undefined;
+let name = prompt("Vad heter du?");
+alert("Hej " + name + "!" )
