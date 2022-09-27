cfaaf27394744c11bd223b18172ec00b7e1363b7
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-27 12:22:25 +0200

    different logging part 1

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,16 +1,6 @@
-let input = prompt("skriv ett ta för att dra av moms");
-let moms = input * 0.8;
-if (input === Number) {
-    console.log("input === number");
-}
-if (input == isNaN) {
-    alert("Du skrev inte in ett nummer.");
-    console.log("isNaN")
-}
-if (input === null) {
-    console.log("input === null")
-}
-else {
-    console.log("else moms")
-    alert(moms);
+let number = 0;
+
+while (number < 3) {
+    console.log(number);
+    number++;
 }
