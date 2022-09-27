bd1f798bd3b74b7d2194dd87f254b37d0557f424
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-27 11:30:34 +0200

    age controll

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,10 +1,6 @@
-let a = 4;
-b = true;
-let c = 2;
-let d = "Hej";
-
-// test
-// console.log(c > 10 ? 'foo' : 'bar');
-// console.log(d.length < a);
-// console.log(d.length > c);
-// console.log(d.length < a && d.length > c);
+let input = prompt ("Ange din ålder");
+if (input >= 18 ) {
+    alert("Vuxen");
+} else {
+    alert("Ungdom");
+}
