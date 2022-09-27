edff7e630d76f9265f8656632e73e720c99b9d58
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-27 06:45:22 +0200

    value restriktions part 2

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,2 +1,10 @@
-let s = "";
-let n = 0;
+let a = 4;
+b = true;
+let c = 2;
+let d = "Hej";
+
+// test
+// console.log(c > 10 ? 'foo' : 'bar');
+// console.log(d.length < a);
+// console.log(d.length > c);
+// console.log(d.length < a && d.length > c);
