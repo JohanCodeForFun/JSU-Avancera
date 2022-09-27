8b12b5f467327fb9ae7d9eecf769cd63c7f81f71
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-27 12:24:49 +0200

    different logging part 2

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,6 +1,10 @@
-let number = 0;
+// let number = 0;
 
-while (number < 3) {
-    console.log(number);
-    number++;
+// while (number < 3) {
+//     console.log(number);
+//     number++;
+// }
+
+for (i = 0; i < 3; i++) {
+    console.log(i);
 }
