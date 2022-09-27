c125b5f034c032ebfbc7bb0beb76494d36d73c7a
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-27 06:37:28 +0200

    truthy and falsy

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,15 +1,2 @@
-let b = false;
-let c = true;
-
-console.log(b === c);
-
-let n = 1.5;
-let o = 3;
-
-console.log(n<2);
-console.log(n+o);
-
-let s = "Hello ";
-let t = "World!";
-console.log(t.length>5);
-console.log(s+t);
+let s = "";
+let n = 0;
