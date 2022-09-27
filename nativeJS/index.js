d8955dcf11e967d74862e8523544c91ff3a2812e
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-27 12:04:20 +0200

    subtract tax part 3 NOT DONE

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,7 +1,16 @@
 let input = prompt("skriv ett ta för att dra av moms");
 let moms = input * 0.8;
+if (input === Number) {
+    console.log("input === number");
+}
+if (input == isNaN) {
+    alert("Du skrev inte in ett nummer.");
+    console.log("isNaN")
+}
 if (input === null) {
-
-} else {
+    console.log("input === null")
+}
+else {
+    console.log("else moms")
     alert(moms);
 }
