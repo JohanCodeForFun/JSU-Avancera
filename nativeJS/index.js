a4dbc784217bf9738173e25d3ee91e0819576fe8
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-28 21:25:29 +0200

    arrays and objects, nested objects part 1

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,3 +1,9 @@
-let a = [
-    ["a"], [ "b", "c"]
-]
+let o = {
+    a: [1]
+};
+
+// at first I wrote, let o = { a: 1 }; 
+// which resulted in, o.a === 1, false.
+// after some thinking and reading i realized,
+// [0] is used to access a position in an array.
+// Then I changed a: 1 to, a: [1] is === 1, true.
