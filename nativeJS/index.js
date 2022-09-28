9f7822e4c6a71ef8e5448fc22e8589b2da7cddac
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-28 22:02:33 +0200

    functions and params, concatenate and log

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,6 +1,4 @@
-let displayDialogBox = function (inputText) {
-    alert(inputText);
+let concatenateAndLog = function (string1, string2) {
+    sumStrings = string1 + string2;
+    console.log(sumStrings);
 }
-
-displayDialogBox("hello");
-displayDialogBox("world");
