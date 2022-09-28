ff2d877fdb2e16cfbe603fdcd9bd12b251f33762
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-28 09:50:18 +0200

    subtract tax part 3

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,10 +1,16 @@
 let input = prompt("skriv ett tal för att dra av moms");
 let moms = input * 0.8;
+
+// funktion, om input är ett nummer, räkna ut moms.
 if (!isNaN(input) && input > 0) {
     alert(moms);
 }
+
+// om användaren stänger rutan utan input, avbrytt.
 else if (input === !null) {
 }
+
+// om användaren anger annat än nummer, ge beskrivande fel meddelande.
 else {
-    if (isNaN(input)) alert("Du skrev inte in ett nummer");
+    if (isNaN(input)) alert("Du skrev inte in ett nummer.");
 }
