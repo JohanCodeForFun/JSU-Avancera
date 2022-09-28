22d65ee9885477e98228bdb350f8b487b2206936
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-28 21:51:16 +0200

    function and params, dialogbox part 1

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,9 +1,6 @@
-let o = {
-    a: [1]
-};
+let displayDialogBox = function () {
+    alert();
+}
 
-// at first I wrote, let o = { a: 1 }; 
-// which resulted in, o.a === 1, false.
-// after some thinking and reading i realized,
-// [0] is used to access a position in an array.
-// Then I changed a: 1 to, a: [1] is === 1, true.
+displayDialogBox();
+displayDialogBox();
