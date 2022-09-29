6cd71a5f682c17c2999b62198b0ba4efdb209075
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-29 11:33:47 +0200

    arrays index from an index DONE!

---
+++ nativeJS/index.js
@@ -0,0 +1,9 @@
+let a = [20, 10, 20];
+
+a[0] = 2;
+// let index = 2;
+// tänk, att ha en variable, där jag berättar
+// index till arrayn a. Räkna därifrån.
+
+// console.log(a[2] + a[1])
+// console.log(a[a[0]] + a[1] === 30)
