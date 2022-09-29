7fac6de9d845b283302f81e7a34eadf1ed77d93f
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-29 13:59:13 +0200

    switch example, get day of week with new date

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,22 +1,27 @@
-let input = prompt("ange nummer du vill ha alert ruta visas");
+// let day = "";
 
-let number;
-if (input === null) {
-  let = 2;
-} else if (input === '') {
-    alert("Skriv in ett nummer");
+switch(new Date().getDay()) {
+    case 1:
+        day = "Monday";
+        console.log(day);
+        break;
+    case 2:
+        day = "Tuesday";
+        console.log(day);
+        break;
+    case 3:
+        day = "Wednesday";
+        console.log(day);
+        break;
+    case 4:
+        day = "Thursday";
+        console.log(day);
+        break;
+    case 5:
+        day = "Friday";
+        console.log(day);
+        break;
+        default:
+        console.log(day);
+        break;
 }
-
-// while (number < input) {
-//     alert(number);
-//     number++;
-// }
-
-
-
-// let number = 0;
-
-// while (number < 3) {
-//     console.log(number);
-//     number++;
-// }
