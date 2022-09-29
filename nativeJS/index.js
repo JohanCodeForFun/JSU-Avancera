16836174c7f806969c48964a8dd3deda83e19722
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-29 20:48:12 +0200

    return value, recursive function (factorial)

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,8 +1,24 @@
 const factorial = function(num) {
-    let result = 1;
-    
-    for (let i = num; i > 0; i--) {
-        result *= i;
-    }
-    return result;
-}
+    return (num === 0 || num === 1) ? 1 : num * factorial(num - 1);
+};
+
+// factorialize with for loop
+// const factorial = function(num) {
+//     let result = 1;
+
+//     for (let i = num; i > 0; i--) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// function with if statement
+// function factorialize(num) {
+
+//     if(num===0){
+//       return 1;
+//     }
+//     else{
+//       return num*factorialize(num-1);
+//     }
+//   }
