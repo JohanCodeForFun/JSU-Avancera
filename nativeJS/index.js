62fe0e80d5bc08a53639f13f60b7d74ffdad07cc
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-29 20:04:32 +0200

    return value, return value

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,10 +1,6 @@
-let number;
-
-while (isNaN(number) || number === null) {
-    number = prompt("Skriv in nummer");
-    if (!isNaN(number) && number !== null) {
-        for (let i = 1; i <= number; i++) {
-            alert("Ruta: " + i + " av " + number)
-        }
-    }
+const greeting = function() {
+    return alert("Hello World!");
 }
+
+let s = greeting();
+alert(s);
