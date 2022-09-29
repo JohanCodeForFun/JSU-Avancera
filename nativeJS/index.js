4e7b4574b887cc86836294dff2205278515445c9
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-09-29 15:05:43 +0200

    if, else, switch, subtract moms (vat) part 3

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,1 +1,21 @@
-let a = [[1, 2, 3, 4, 5],[]];
+let input = prompt("skriv ett tal för att dra av moms");
+let moms = input * 0.8;
+if (!isNaN(input) && input > 0) {
+    alert(moms);
+}
+else if (input === !null) {
+}
+else {
+    if (isNaN(input)) alert("Du skrev inte in ett nummer.");
+}
+
+//alt lösning
+// const n = prompt()
+
+// if (n !== null) {
+//     if (isNaN(n)) {
+//         alert("Du skrev...")
+//     } else {
+//         alert(n * 0.8)
+//     };
+// };
