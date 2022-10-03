8651244a91461963ac7cd6840ed45a367e433636
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-03 19:08:31 +0200

    functions average

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,8 +1,13 @@
-let sum = function(array) {
+let average = function(array) {
     let result = 0;
-    
+
+    if (array.length === 0) {
+        return null;
+    }
+
     for (let i = 0; i < array.length; i++) {
         result += array[i];
     }
+    result /= array.length;
     return result;
 }
