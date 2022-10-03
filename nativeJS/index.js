7baebee5194646c153d2399b088599cab08ddd56
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-03 10:51:55 +0200

    loop through arrays with for loops

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,3 +1,14 @@
-let a = [
-    '', 'ab', 'c', 
-];
+let firstNames = ["Richard", "Simon", "Arvid"];
+
+for (let i = 0; i < firstNames.length; i++) {
+    console.log(firstNames[i]);
+}
+
+let a = [1, 2, 3, 4, 5];
+a = [6, 7, 8, 9, 10]
+
+console.log(a);
+
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
