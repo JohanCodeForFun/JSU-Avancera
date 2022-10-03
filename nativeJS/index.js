836ed02d3a6fe93ea6e52f90c01538335260ac5a
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-03 21:18:43 +0200

    function declarations, an function declaration

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,12 +1,3 @@
-let average = function(array) {
-    let result = 0;
-
-    if (array.length === 0) {
-        return null;
-    }
-
-    array.forEach(function(item) {
-    result += item;        
-    });
-    return result / array.length;
+function greeting() {
+    return 'Hello World!'
 }
