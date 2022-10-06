af15a0bf4b51f0bdbfa8051f20d42a6c116b0854
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-06 13:12:05 +0200

    blackjack-logik, string representation

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,38 +1,79 @@
-const prettyCard = function (card) {
-  let suit, value
-
-  switch (suit) {
-    case 'HEARTS':
-      "&hearts";
-      break;
-    case 'SPADES':
-      "&spades";
-      break;
-    case 'DIAMONDS':
-      "&diams";
-      break;
-    case 'CLUBS':
-      "&clubs";
-      break;
-  
-    default:
-      break;
-  }
+const prettyCard = function (suit, value) {
 
   switch (value) {
-    case "A":
-      1
+    case 1:
+      value = "A";
       break;
     case 2:
-      2
+      value = 2;
+      break;
+    case 3:
+      value = 3;
+      break;
+    case 4:
+      value = 4;
+      break;
+    case 5:
+      value = 5;
+      break;
+    case 6:
+      value = 6;
+      break;
+    case 7:
+      value = 7;
+      break;
+    case 8:
+      value = 8;
+      break;
+    case 9:
+      value = 9;
+      break;
+    case 10:
+      value = "T";
+      break;
+    case 11:
+      value = "J";
+      break;
+    case 12:
+      value = "Q";
+      break;
+    case 13:
+      value = "K";
       break;
   
     default:
       break;
   }
+  
+  switch (suit) {
+    case 'HEARTS':
+      suit = "♥";
+      break;
+    case 'SPADES':
+      suit = "♠";
+      break;
+    case 'DIAMONDS':
+      suit = "♦";
+      break;
+    case 'CLUBS':
+      suit = "♣";
+      break;
+  
+    default:
+      break;
+  }
+
+  
 
   return value + suit
 }
+
+
+
+console.log(prettyCard('HEARTS', 1));
+console.log(prettyCard('SPADES', 2));
+console.log(prettyCard('DIAMONDS', 10));
+console.log(prettyCard('CLUBS', 11));
 
 // test case,
 // console.log(prettyCard({ suit: 'HEARTS', value: 1 }), 'A♥')
