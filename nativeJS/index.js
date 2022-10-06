f8f81792a054672fb40de10bb4d699e141fe58a0
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-06 20:33:40 +0200

    blackjack-logic, a deck of cards done!

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,35 +1,40 @@
-// return object: card, value & suit 
-let card = (value, suit) => {
-  return {
-    value,
-    suit
-  }
-}
-
-console.log(card(2, 3));
-
-
-//push hearths cards into array
-let hearts = [];
-
-// create push loop to fill hearts deck
-for (let i = 1; i <= 13; i++) {
-  hearts.push({ suit: 'HEARTS', value: i })
-}
-
-console.log(hearts);
-
-
 // build deck assignment
 let deck = [];
 
 let suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS']
 
 for (let i = 0; i < suits.length; i++) {
-
-  // console.log(suits[i]);
-  console.log(suits[i]);
+  for (let j = 0; j < 13; j++) {
+    deck.push({ suit: suits[i], value: j + 1 })
+  }
 }
+
+console.log(deck);
+
+
+// Learning experiences that made it possible to complete assignment:
+
+// return object: card, value & suit 
+// let card = (value, suit) => {
+//   return {
+//     value,
+//     suit
+//   }
+// }
+
+// console.log(card(2, 3));
+
+
+//push hearths cards into array
+// let hearts = [];
+
+// // create push loop to fill hearts deck
+// for (let i = 1; i <= 13; i++) {
+//   hearts.push({ suit: 'HEARTS', value: i })
+// }
+
+// console.log(hearts);
+
 
 // create push loop to fill deck of cards
 // for (let i = 0; i < suits.length; i++) {
