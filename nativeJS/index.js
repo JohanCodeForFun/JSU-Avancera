773e2957642cdf94920791982735d80611de209b
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-06 05:28:30 +0200

    blackjack-logic, all ace of hearts

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,6 +1,26 @@
-  let card = (value, suit) => {
-  return {
-    value,
-    suit
-  }
+let hearts = [];
+
+// create push loop to fill hearts deck
+for (let i = 1; i <= 13; i++) {
+  hearts.push({ suit: 'HEARTS', value: i })
 }
+
+// hearts.push({  })
+// hearts.push({ suit: 'HEARTS', value: 1 })
+
+// desired outcome:
+// let hearts = [
+//     { suit: 'HEARTS', value: 1 },
+//     { suit: 'HEARTS', value: 2 },
+//     { suit: 'HEARTS', value: 3 },
+//     { suit: 'HEARTS', value: 4 },
+//     { suit: 'HEARTS', value: 5 },
+//     { suit: 'HEARTS', value: 6 },
+//     { suit: 'HEARTS', value: 7 },
+//     { suit: 'HEARTS', value: 8 },
+//     { suit: 'HEARTS', value: 9 },
+//     { suit: 'HEARTS', value: 10 },
+//     { suit: 'HEARTS', value: 11 },
+//     { suit: 'HEARTS', value: 12 },
+//     { suit: 'HEARTS', value: 13 }
+// ];
