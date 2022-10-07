1651f6a5a253fc778840924bf4a50dd8e56c5600
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-07 12:14:50 +0200

    functions of higher order, find (continued)

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,7 +1,27 @@
 // function to use as second paramater, 
 // for example: opening a prompt window.
-let f = () => g();
 
+const array1 = [1, 2, 3, 4, 5];
+
+// loopa igenom med for loop
+// funktion 
+// få fram högsta värdet i arrayn
+// inne i loopen, if sats, 
+// // ta reda på när F är true
+// // när nyckeln i loopen är true, return true nyckel värde
+// returnera värdet
+
+function filter(value) {
+  console.log(array1.filter(value => value > 2));
+
+}
+
+filter(13);
+
+
+
+// let f = () => includes();
+// console.log(f([]))
 
 // function to go through an array and 
 // propmt user with the values.
