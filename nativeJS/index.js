34b515ec9b7e2c1b0ed2748955de3f1ddf2a7c1d
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-07 11:13:57 +0200

    functions of higher order, for each function

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,7 +1,17 @@
-// Arrow function, ES6
-let f = () => g;
-let g = () => 'Hello World!';
+// function to use as second paramater, 
+// for example: opening a prompt window.
+let f = () => g();
 
+
+// function to go through an array and 
+// propmt user with the values.
+function forEach(a, f) {
+  for (let i = 0; i < a.length; i++) {
+    f((a[i]));
+  }
+};
+
+// forEach([1, 2, 3, 4, 5], console.log);
 
 // old way
 // let f = function() {
