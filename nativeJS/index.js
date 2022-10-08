9604113c05e68ee9d84f445d80fb4534df06ed39
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-08 16:53:04 +0200

    functions of higher order, find

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,87 +1,10 @@
-'use strict';
+// create a function that takes in an array,
+// and return the max value in that array.
+// Example, index.js defines find such that it returns 15 
+// when called with find([5, 10, 15], n => n === 15).
 
-// Blackjack Logic, Function return a deck of cards
+function find(a) {
+  return Math.max(...a);
+};
 
-
-let suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS']
-
-function createDeck() {
-  let deck = [];
-  
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < 13; j++) {
-      deck.push({ suit: suits[i], value: j + 1 })
-    }
-  }
-  return deck;
-}
-
-console.log(createDeck().length);
-console.log(createDeck().length);
-console.log(createDeck().length);
-
-
-// Learning experiences that made it possible to complete assignment:
-
-// return object: card, value & suit 
-// let card = (value, suit) => {
-//   return {
-//     value,
-//     suit
-//   }
-// }
-
-// console.log(card(2, 3));
-
-
-//push hearths cards into array
-// let hearts = [];
-
-// // create push loop to fill hearts deck
-// for (let i = 1; i <= 13; i++) {
-//   hearts.push({ suit: 'HEARTS', value: i })
-// }
-
-// console.log(hearts);
-
-
-// create push loop to fill deck of cards
-// for (let i = 0; i < suits.length; i++) {
-//   for (let j = 1; i < 13; j++) {
-//   deck.push({ suit: suits[i], value: i })
-//   }
-// }
-
-// hearts.push({  })
-// hearts.push({ suit: 'HEARTS', value: 1 })
-
-// let deck = [];
-
-// let suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS'];
-
-
-// // create push loop to fill hearts deck
-// for (let i = 1; i <= 13; i++) {
-//   deck.push({ suit: 'HEARTS', value: i })
-// }
-
-// hearts.push({  })
-// hearts.push({ suit: 'HEARTS', value: 1 })
-
-
-// desired outcome:
-// let hearts = [
-//     { suit: 'HEARTS', value: 1 },
-//     { suit: 'HEARTS', value: 2 },
-//     { suit: 'HEARTS', value: 3 },
-//     { suit: 'HEARTS', value: 4 },
-//     { suit: 'HEARTS', value: 5 },
-//     { suit: 'HEARTS', value: 6 },
-//     { suit: 'HEARTS', value: 7 },
-//     { suit: 'HEARTS', value: 8 },
-//     { suit: 'HEARTS', value: 9 },
-//     { suit: 'HEARTS', value: 10 },
-//     { suit: 'HEARTS', value: 11 },
-//     { suit: 'HEARTS', value: 12 },
-//     { suit: 'HEARTS', value: 13 }
-// ];
+// console.log(find([5, 10, 15]));
