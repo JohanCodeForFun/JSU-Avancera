7120ff11c482355b0eeb34622dab731464395780
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-10 14:01:51 +0200

    Update class.js

--- nativeJS/class.js
+++ nativeJS/class.js
@@ -1,6 +1,29 @@
+
+
+let repeat = function(string, num) {
+    let repeatedString = [];
+    if (num > 0) {
+        for(let i = 0; i < num; i++) {
+            repeatedString.push(string);
+        }
+        return repeatedString;
+    }
+}
+
+
+
+
+
+
+
+
 let numbers = [1, 2, 3, 4, 5];
 
 let f1 = function (numbers) {
     return number+1
 }
-console.log(numbers.map(f1));
+// console.log(numbers.map(f1));
+console.log(f1());
+
+
+
