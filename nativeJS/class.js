082069bb688fa2754f6d3e9cd95013fe3f43e100
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-10 21:43:00 +0200

    Update class.js

--- nativeJS/class.js
+++ nativeJS/class.js
@@ -1,22 +1,3 @@
-
-
-let repeat = function(string, num) {
-    let repeatedString = [];
-    if (num > 0) {
-        for(let i = 0; i < num; i++) {
-            repeatedString.push(string);
-        }
-        return repeatedString;
-    }
-}
-
-
-
-
-
-
-
-
 let numbers = [1, 2, 3, 4, 5];
 
 let f1 = function (numbers) {
