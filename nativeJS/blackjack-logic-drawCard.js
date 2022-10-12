0f86ddb63489ada818826c6636cb810f8670eab9
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-12 13:31:50 +0200

    Create blackjack-logic-drawCard.js

---
+++ nativeJS/blackjack-logic-drawCard.js
@@ -0,0 +1,11 @@
+// deck = [1, 2, 3, 4, 5]
+// console.log(deck);
+
+let draw = (deck) => {
+    let card = deck[0];
+    deck.shift();
+    return card;
+  }
+  // draw(deck);
+  
+  // console.log(deck);
