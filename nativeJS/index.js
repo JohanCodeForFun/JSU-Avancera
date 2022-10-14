ce91531799d7c92ce61c30b4dc42b25648b96a0d
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-14 11:27:29 +0200

    GET call with fetch, all cities

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,9 +1,13 @@
-let filter = (arr, f) => {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (f(arr[i])) {
-      newArray.push(arr[i])
-    };
-  };
-  return newArray;
-};
+let promise = fetch('https://avancera.app/cities/');
+
+promise
+  .then(response => {
+    console.log(response)
+
+    let someOtherPromise = response.json();
+
+    return someOtherPromise
+  })
+  .then(result => {
+    console.log(result)
+  });
