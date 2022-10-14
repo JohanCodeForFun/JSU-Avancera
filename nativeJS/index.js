823dbd68e55c248a4fe191f72621894940a99ff2
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-14 11:42:59 +0200

    GET call with fetch, one query parameter

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,7 +1,7 @@
-let inputID = prompt();
-console.log(inputID);
+let searchCityName = prompt();
+console.log(searchCityName);
 
-let promise = fetch(`https://avancera.app/cities/${inputID}`)
+let promise = fetch(`https://avancera.app/cities/?name=${searchCityName}`)
   .then(response => response.json())
   .then(result => {
     console.log(result)
