916a14acffe101a23c90f2683cbee1e96a25727d
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-17 21:11:13 +0200

    DOM nodes, document.querySelector

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,10 +1,1 @@
-let element = document.querySelector('#some-element')
-console.log(element.children) // Barnelement
-console.log(element.childNodes) // Barnnoder
-
-// let parapgraphs = document.querySelectorAll('p');
-// console.log(parapgraphs);
-
-// for (let i = 0; i < parapgraphs.length; i++ ) {
-//   console.log(parapgraphs[i])
-// }
+let item = document.querySelector("#players"); 
