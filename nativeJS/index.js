42db874cca980a2ecbc5ae0ed95aca956e6c48aa
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-17 11:16:54 +0200

    more about fetch, POST, add a city

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,5 +1,14 @@
-fetch('https://avancera.app/cities/?name=' + prompt() + "&minPopulation=" + Number(prompt()))
+let createCity = prompt("lägg till en stad");
+let setPopulation = Number(prompt("Läggg till befolkningsmängd"));
+
+fetch('https://avancera.app/cities/', {
+  body: `{ "name": "${createCity}", "population": ${setPopulation} }`,
+  headers: {
+    'Content-type': 'application/json'
+  },
+  method: 'POST'
+})
   .then(response => response.json())
   .then(result => {
     console.log(result)
-  })
+  });
