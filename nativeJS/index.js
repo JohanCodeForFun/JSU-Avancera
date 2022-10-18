16cf75493073a646069c8997bff1462aaced096d
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-18 12:02:50 +0200

    DOM nodes, one element loop ES6

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,6 +1,10 @@
-let list = document.querySelectorAll('li');
-
-for (const item of list) {
-  item.innerHTML += '<span class="credits">100 credits</span>'
+let items = document.querySelectorAll("li");
+//ES6 loop
+for (const item of items) {
   console.log(item);
 }
+
+// old way, for loop
+// for (let i = 0; i < items.length; i++) {
+//   console.log(items[i]);
+// }
