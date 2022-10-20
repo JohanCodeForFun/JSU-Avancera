3bdccd3ddbc54c0a1e0f8eb9aee7173ead4eeb5f
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-20 21:44:32 +0200

    Update index.js

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,8 +1,7 @@
 const parent = document.querySelector('ol');
-
 const lastChildNode = parent.children[parent.children.length-1]
-const listNode = document.createElement("li");
 
+const listNode = document.createElement("li");
 const textNode = document.createTextNode("Dennis");
 
 parent.appendChild(listNode);
