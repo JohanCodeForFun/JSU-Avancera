04e3729c894794357fa6060b3a26df0c291a000c
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-20 20:11:27 +0200

    DOM notes from class

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,10 +1,51 @@
-let items = document.querySelectorAll("li");
-//ES6 loop
-for (const item of items) {
-  console.log(item);
-}
+let getCityId = prompt("Ange ID för den stad du vill uppdatera");
+// let changeName = prompt("Ändra namn för staden");
+// let changePopulation = Number(prompt("Uppdatera befolkningsmängd"));
 
-// old way, for loop
-// for (let i = 0; i < items.length; i++) {
-//   console.log(items[i]);
-// }
+const listdfd = document.querySelectorAll('li');
+const ol1 = document.querySelector('ol');
+listdfd.forEach(listItem => {console.log(listItem)});
+console.log("log list [2], " + listdfd[2])
+
+listdfd[2].textContent = "Clara";
+
+const people = ['Eddie', 'Klara', 'Johan'];
+
+people.forEach(person => {
+  ol1.innerHTML += `<li> ${person} </li>`
+})
+
+
+if (getCityId !== null || getCityId !== undefined || getCityId !== "") {
+  console.log("inside first if");
+
+  if (changeName === null || changeName === undefined) {
+    console.log("uppdatera befolkning prompt");
+
+    if (changePopulation === null || changePopulation === undefined) {
+      console.log("byt namn prompt");
+    }
+
+    // } else if (changePopulation === null || changePopulation === undefined) {
+  // } else if (){
+  //   console.log("Updatera namn och befolkning");
+  
+  } else { {
+    console.log("byt namn & stad prompt");
+  }
+}}
+
+
+// fetch('https://avancera.app/cities/' + getCityId, {
+//   body: JSON.stringify({
+//     "name": changeName,
+//     "population": changePopulation
+//   }),
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   method: 'PATCH',
+// })
+//   .then(result => {
+//     console.log(result)
+//   });
