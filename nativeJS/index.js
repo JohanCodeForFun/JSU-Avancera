a1e541ff11bdb5a1f48fe9cf0debbca133433d36
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-21 15:06:24 +0200

    JS Global addEventListener, increase or decrease one value: Part 3 (VG)

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,6 +1,49 @@
-// Modul: Events och addEventListener
+// Modul: "Global" Events och addEventListener
 
-// const body1 = document.body;
-addEventListener('click', () => {
-  console.log(event.pageX);
+addEventListener("keydown", () => {
+  if (event.keyCode === 38) {
+    valueSum++;
+    value.textContent = valueSum;
+    if (valueSum > 1) {
+      decreaseBtn.disabled = false;
+    }
+  }
+  if (event.keyCode === 40) {
+    if (valueSum === 1) {
+      decreaseBtn.disabled = true;
+      return false;
+    } else {
+      valueSum--;
+      value.textContent = valueSum;
+      if (valueSum === 1) {
+        decreaseBtn.disabled = true;
+      }
+    }
+  }
+});
+
+const value = document.querySelector("#value");
+const increaseBtn = document.querySelector("#increase");
+const decreaseBtn = document.querySelector("#decrease");
+
+let valueSum = 1;
+value.textContent = valueSum;
+
+if (valueSum === 1) {
+  decreaseBtn.disabled = true;
+}
+
+increaseBtn.addEventListener("click", () => {
+  valueSum++;
+  value.textContent = valueSum;
+  if (valueSum > 1) {
+    decreaseBtn.disabled = false;
+  }
+});
+decreaseBtn.addEventListener("click", () => {
+  valueSum--;
+  value.textContent = valueSum;
+  if (valueSum === 1) {
+    decreaseBtn.disabled = true;
+  }
 });
