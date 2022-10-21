26afe73474419c70f977f445252f9a2bac5f4c3a
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-21 12:18:26 +0200

    JS Events and addEventListener, increase or decrease one value: Part 1

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -2,11 +2,16 @@
 
 const value = document.querySelector('#value');
 const increaseBtn = document.querySelector('#increase');
+const decreaseBtn = document.querySelector('#decrease');
 
 let valueSum = 1;
 value.textContent = valueSum;
 
 increaseBtn.addEventListener('click', () => {
   valueSum++;
+  value.textContent = valueSum;
+});
+decreaseBtn.addEventListener('click', () => {
+  valueSum--;
   value.textContent = valueSum;
 });
