4d7cc61cfaa3f20edf771d29850863f9cc3b40ce
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-21 11:58:43 +0200

    clear file

--- nativeJS/index.js
+++
@@ -1,9 +0,0 @@
-const parent = document.querySelector('ol');
-const lastChildNode = parent.children[parent.children.length-1]
-
-const listNode = document.createElement("li");
-const textNode = document.createTextNode("Dennis");
-
-parent.appendChild(listNode);
-listNode.appendChild(textNode);
-parent.insertBefore(listNode, lastChildNode);
