92715459e0f7a8187c8e5d1ea2593b38642b7eef
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-21 12:43:43 +0200

    JS Global addEventListener, a clickable button part: 2

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,27 +1,9 @@
 // Modul: Events och addEventListener
 
-const value = document.querySelector('#value');
-const increaseBtn = document.querySelector('#increase');
-const decreaseBtn = document.querySelector('#decrease');
+addEventListener("DOMContentLoaded", () => {
 
-let valueSum = 1;
-value.textContent = valueSum;
-
-if (valueSum === 1) {
-  decreaseBtn.disabled = true;
-}
-
-increaseBtn.addEventListener('click', () => {
-  valueSum++;
-  value.textContent = valueSum;
-  if (valueSum > 1) {
-    decreaseBtn.disabled = false;
-  }
-});
-decreaseBtn.addEventListener('click', () => {
-  valueSum--;
-  value.textContent = valueSum;
-  if (valueSum === 1) {
-    decreaseBtn.disabled = true;
-  }
+  const btn = document.querySelector("#button");
+  btn.addEventListener("click", () => {
+    console.log("click");
+  });
 });
