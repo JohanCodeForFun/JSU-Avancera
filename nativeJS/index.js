b973a1861c0d08f897b86415b852ee5ab6f734c3
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-23 20:11:30 +0200

    Get input values, a checkbox (NOT COMPLETE)

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,20 +1,44 @@
 let inputValue = "";
+let inputName = document.querySelector("#name")
 let sendBtn = document.querySelector("#send");
+let checkBox = document.querySelector('input[type="checkbox"]');
+
+let errorDiv = document.querySelector("#errors");
+let errorName = document.querySelector("#name-error");
+let errorConsent = document.querySelector("#consent-error");
+
 sendBtn.disabled = true;
-let errorDiv = document.querySelector('#errors');
 errorDiv.style.display = "inline";
 
-document.querySelector("#name").addEventListener("input", () => {
+inputName.addEventListener("input", () => {
   inputValue = document.querySelector("#name").value;
 
   if (inputValue === "") {
     sendBtn.disabled = true;
-    errorDiv.style.display = "inline";
+    errorName.style.display = "inline";
+    errorConsent.style.display = "list-item";
   } else {
     sendBtn.disabled = false;
-    errorDiv.style.display = "none";
+    errorName.style.display = "none";
   }
 });
+
+checkBox.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    errorConsent.style.display = "none";
+    
+    if (e.target.checked && inputValue !== "") {
+      errorDiv.style.display = "none";
+    }
+  } else {
+    errorConsent.style.display = "list-item";
+  }
+});
+
+// how do i have this as listen on change?
+// if (e.target.checked && inputValue !== "") {
+//   errorDiv.style.display = "none";
+// }
 
 let greeting = document.querySelector("#greeting");
 let submitBtn = document.querySelector("#send");
