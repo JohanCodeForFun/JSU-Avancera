04acfe5d0f6b60cc993c3c7d67050c94b9fd61cb
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-23 16:58:56 +0200

    Get input values, an invalid value

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,5 +1,23 @@
-let greeting = document.querySelector('#greeting');
-let submitBtn = document.querySelector('#send');
-submitBtn.addEventListener('click', () => {
-  greeting.textContent = "Hej " + document.querySelector('#name').value + "!"
+let inputValue = "";
+let sendBtn = document.querySelector("#send");
+sendBtn.disabled = true;
+let errorDiv = document.querySelector('#errors');
+errorDiv.style.display = "inline";
+
+document.querySelector("#name").addEventListener("input", () => {
+  inputValue = document.querySelector("#name").value;
+
+  if (inputValue === "") {
+    sendBtn.disabled = true;
+    errorDiv.style.display = "inline";
+  } else {
+    sendBtn.disabled = false;
+    errorDiv.style.display = "none";
+  }
+});
+
+let greeting = document.querySelector("#greeting");
+let submitBtn = document.querySelector("#send");
+submitBtn.addEventListener("click", () => {
+  greeting.textContent = "Hej " + document.querySelector("#name").value + "!";
 });
