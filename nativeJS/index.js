dc677d15897008820faa8dfeb3550b04248f4da7
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-24 20:51:16 +0200

    more about fetch, update name and/or population

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,47 +1,20 @@
-let inputValue = "";
-let inputName = document.querySelector("#name")
-let sendBtn = document.querySelector("#send");
-let checkBox = document.querySelector('input[type="checkbox"]');
+const id = prompt(),
+  name = prompt(),
+  population = prompt(),
+  body = {}
 
-let errorDiv = document.querySelector("#errors");
-let errorName = document.querySelector("#name-error");
-let errorConsent = document.querySelector("#consent-error");
+if (name !== null) {
+  body.name = name
+}
 
-sendBtn.disabled = true;
-errorDiv.style.display = "inline";
+if (population !== null) {
+  body.population = Number(population)
+}
 
-inputName.addEventListener("input", () => {
-  inputValue = document.querySelector("#name").value;
-
-  if (inputValue === "") {
-    sendBtn.disabled = true;
-    errorName.style.display = "inline";
-    errorConsent.style.display = "list-item";
-  } else {
-    sendBtn.disabled = false;
-    errorName.style.display = "none";
-  }
-});
-
-checkBox.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    errorConsent.style.display = "none";
-    
-    if (e.target.checked && inputValue !== "") {
-      errorDiv.style.display = "none";
-    }
-  } else {
-    errorConsent.style.display = "list-item";
-  }
-});
-
-// how do i have this as listen on change?
-// if (e.target.checked && inputValue !== "") {
-//   errorDiv.style.display = "none";
-// }
-
-let greeting = document.querySelector("#greeting");
-let submitBtn = document.querySelector("#send");
-submitBtn.addEventListener("click", () => {
-  greeting.textContent = "Hej " + document.querySelector("#name").value + "!";
-});
+fetch(`https://avancera.app/cities/${id}`, {
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  method: 'PATCH'
+})
