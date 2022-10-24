f0f7c2b118c9d735114bb21b464b006455caf7b4
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-24 22:31:32 +0200

    Update index.js

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,20 +1,42 @@
-const id = prompt(),
-  name = prompt(),
-  population = prompt(),
-  body = {}
+let inputValue = "";
+let inputName = document.querySelector("#name")
+let sendBtn = document.querySelector("#send");
+let checkBox = document.querySelector('input[type="checkbox"]');
 
-if (name !== null) {
-  body.name = name
+let errorDiv = document.querySelector("#errors");
+let errorName = document.querySelector("#name-error");
+let errorConsent = document.querySelector("#consent-error");
+
+sendBtn.disabled = true;
+errorDiv.style.display = "inline";
+
+inputName.addEventListener("input", () => {
+  inputValue = document.querySelector("#name").value;
+
+  if (inputValue === "") {
+    sendBtn.disabled = true;
+    errorName.style.display = "inline";
+    errorConsent.style.display = "list-item";
+  } else {
+    sendBtn.disabled = false;
+    errorName.style.display = "none";
+  }
+});
+
+let checkBoxFn = (e) => {
+  if (e.target.checked) {
+    errorConsent.style.display = "none";
+  } else {
+    errorConsent.style.display = "list-item";
+  }
 }
 
-if (population !== null) {
-  body.population = Number(population)
+let consentFN = (e) => {
+  if (e.target.checked && inputValue !== "") {
+    errorDiv.style.display = "none";
+  }
 }
 
-fetch(`https://avancera.app/cities/${id}`, {
-  body: JSON.stringify(body),
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  method: 'PATCH'
-})
+checkBox.addEventListener("change", checkBoxFn, consentFN => {});
+
+// hur kan jag lyssna på att båda är checked?
