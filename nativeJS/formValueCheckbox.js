1038264026094a577da1cbb99aa1a17212630685
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-24 20:50:54 +0200

    Create formValueCheckbox.js

---
+++ nativeJS/formValueCheckbox.js
@@ -0,0 +1,50 @@
+let inputValue = "";
+let inputName = document.querySelector("#name")
+let sendBtn = document.querySelector("#send");
+let checkBox = document.querySelector('input[type="checkbox"]');
+
+let errorDiv = document.querySelector("#errors");
+let errorName = document.querySelector("#name-error");
+let errorConsent = document.querySelector("#consent-error");
+
+sendBtn.disabled = true;
+errorDiv.style.display = "inline";
+
+// titta på hur jag kan använda loopa eller lyssna förändring när
+// båda är icheckade
+
+inputName.addEventListener("input", () => {
+  inputValue = document.querySelector("#name").value;
+
+  if (inputValue === "") {
+    sendBtn.disabled = true;
+    errorName.style.display = "inline";
+    errorConsent.style.display = "list-item";
+  } else {
+    sendBtn.disabled = false;
+    errorName.style.display = "none";
+  }
+});
+
+checkBox.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    errorConsent.style.display = "none";
+    
+    if (e.target.checked && inputValue !== "") {
+      errorDiv.style.display = "none";
+    }
+  } else {
+    errorConsent.style.display = "list-item";
+  }
+});
+
+// how do i have this as listen on change?
+// if (e.target.checked && inputValue !== "") {
+//   errorDiv.style.display = "none";
+// }
+
+let greeting = document.querySelector("#greeting");
+let submitBtn = document.querySelector("#send");
+submitBtn.addEventListener("click", () => {
+  greeting.textContent = "Hej " + document.querySelector("#name").value + "!";
+});
