760f3bf61b0b6acd3d7ea3fdb9fa8bac170d44f2
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-26 21:17:00 +0200

    JS Forms, a text field, paragraph and a button

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,1 +1,9 @@
-document.querySelector('#name').value = "Alice";
+let inputField = document.querySelector('#name');
+let submitBtn = document.querySelector('#send');
+let greetingPara = document.querySelector('#greeting');
+
+let submitFunction = () => {
+   greetingPara.textContent = `Hej ${inputField.value}!`
+}
+
+submitBtn.addEventListener('click', submitFunction)
