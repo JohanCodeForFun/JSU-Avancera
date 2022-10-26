e152e3581ad9ebea86f98e159daf8424bddb16e9
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-26 21:17:57 +0200

    JS Forms, an invalid value

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,9 +1,24 @@
 let inputField = document.querySelector('#name');
 let submitBtn = document.querySelector('#send');
 let greetingPara = document.querySelector('#greeting');
+let divErrors = document.querySelector('#errors');
 
 let submitFunction = () => {
    greetingPara.textContent = `Hej ${inputField.value}!`
 }
 
+let confirmInput = () => {
+  if (inputField.value === '') {
+    divErrors.style.display = "inline";
+    submitBtn.disabled = true;
+  } else {
+    divErrors.style.display = "none";
+    submitBtn.disabled = false;
+  }
+}
+
+divErrors.style.display = "inline";
+submitBtn.disabled = true;
+
 submitBtn.addEventListener('click', submitFunction)
+addEventListener('input', confirmInput)
