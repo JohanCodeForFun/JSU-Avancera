533f8f21b910b9af673f991be3b1cb903cca608f
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-01-09 22:05:36 +0100

    Introduktion 3, Uppgift: Fem primitiva “variabler”

--- JavaScriptMedRamverk/index.js
+++ JavaScriptMedRamverk/index.js
@@ -2,6 +2,10 @@ Vue.createApp({
   data() {
     return {
       name: "Alice",
+      number: 9000,
+      waterIsWet: true,
+      lifeOnMars: undefined,
+      sizeOfSpace: null,
     };
   },
 }).mount("#app");
